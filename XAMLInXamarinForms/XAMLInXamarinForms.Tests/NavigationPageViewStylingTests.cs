d94416cefa6288964c9842b9c9cceb1ce88e1cf7
Eric J Fisher <eric-fisher@pluralsight.com>
2020-05-26 12:33:09 -0400

    Reorganized tests slightly to align with modules more than traditional test organization

--- XAMLInXamarinForms/XAMLInXamarinForms.Tests/NavigationPageViewStylingTests.cs
+++ XAMLInXamarinForms/XAMLInXamarinForms.Tests/NavigationPageViewStylingTests.cs
@@ -4,58 +4,8 @@ using Xunit;
 
 namespace XAMLInXamarinForms.Tests
 {
-    public class NavigationPageViewTests
+    public class NavigationPageViewStylingTests
     {
-        [Fact(DisplayName = "Add Click Events To View @add-click-events-to-view")]
-        public void AddClickEventsToView()
-        {
-            var filePath = ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + ".." + Path.DirectorySeparatorChar + "XAMLInXamarinForms" + Path.DirectorySeparatorChar + "Views" + Path.DirectorySeparatorChar + "NavigationPageView.xaml";
-            Assert.True(File.Exists(filePath), "`NavigationPageView` was not found in the `Views" + Path.DirectorySeparatorChar + "Fitness` directory.");
-
-            var xml = File.ReadAllText(filePath);
-            var xmlDocument = new XmlDocument();
-            xmlDocument.LoadXml(xml);
-
-            var contentPage = xmlDocument.DocumentElement;
-            Assert.True(contentPage != null, "`NavigationPageView` does not appear to contain any xaml code.");
-
-            bool fitnessClickEvent = false;
-            bool fitnessFound = false;
-            bool nutritionClickEvent = false;
-            bool nutritionFound = false;
-
-            XmlNodeList children = contentPage.ChildNodes; // supressing unassigned variable error
-            if (contentPage.ChildNodes.Count == 1)
-                children = contentPage.ChildNodes[0]?.ChildNodes[0]?.ChildNodes;
-            else if (contentPage.ChildNodes.Count == 2)
-                children = contentPage.ChildNodes[1]?.ChildNodes[0]?.ChildNodes;
-            else
-                Assert.True(false, "`NavigationPageView`'s `ContentPage` tag should contain only `ContentPage.Content` and `ContentPage.Resources` tags.");
-
-            foreach (XmlElement child in children)
-            {
-                if (child.GetAttribute("x:Name") == "FitnessButton")
-                {
-                    fitnessFound = true;
-                    if(child.GetAttribute("Clicked") == "FitnessButton_Clicked")
-                        fitnessClickEvent = true;
-                }
-
-                if (child.GetAttribute("x:Name") == "NutritionButton")
-                {
-                    nutritionFound = true;
-                    if (child.GetAttribute("Clicked") == "NutritionButton_Clicked")
-                        nutritionClickEvent = true;
-                }
-            }
-
-            Assert.True(fitnessFound, @"`NavigationPageView`'s `FitnessButton` could not be found it should be nested in the `ContentPage` tag's `ContentPage.Content` (this test will fail if you move where the button is located)");
-            Assert.True(fitnessClickEvent, @"`NavigationPageView`'s `FitnessButton` does not appear to have an attribute `Clicked` with a value of ""FitnessButton_Clicked"".");
-            Assert.True(nutritionFound, @"`NavigationPageView`'s `NutritionButton` could not be found it should be nested in the `ContentPage` tag's `ContentPage.Content` (this test will fail if you move where the button is located)");
-            Assert.True(nutritionClickEvent, @"`NavigationPageView`'s `NutritionButton` does not appear to have an attribute `Clicked` with a value of ""NutritionButton_Clicked"".");
-        }
-
-
         [Fact(DisplayName = "Add Button Styles @add-button-styles")]
         public void AddButtonStyles()
         {
