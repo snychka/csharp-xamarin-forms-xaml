399ee574586236f1ff7319b9653716481cd2a8f0
Eric J Fisher <eric-fisher@pluralsight.com>
2020-05-26 21:42:43 -0400

    Added missing x:Name properties, split tests to properly match modules

---
+++ XAMLInXamarinForms/XAMLInXamarinForms.Tests/NavigationPageViewModalTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+using System.Linq;
+using XAMLInXamarinForms.Tests.Helpers;
+using Xunit;
+
+namespace XAMLInXamarinForms.Tests
+{
+    public class NavigationPageViewModalTests
+    {
+        static string userCodeFile = "Views" + Path.DirectorySeparatorChar + "NavigationPageView.xaml.cs";
+        static SyntaxNode ast = TestHelpers.GetUserAst(userCodeFile);
+
+        [Fact(DisplayName = "Add Privacy Modal @add-privacy-modal")]
+        public void AddPrivacyModal()
+        {
+            var methods = TestHelpers.GetMethods(ast);
+            var method = methods?.FirstOrDefault(x => x.ToString().Contains("PopModal"));
+            Assert.True(method != null, "`NavigationPageView.xaml.cs` does not appear to contain a `private` `async` method `PopModal` with a `void` return type and has no parameters.");
+            Assert.True(method.Modifiers.Any(SyntaxKind.PrivateKeyword), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have the `private` modifier.");
+            Assert.True(method.Modifiers.Any(SyntaxKind.AsyncKeyword), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have the `async` modifier.");
+            Assert.True(method.ReturnType.ToString().Contains("void"), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have a return type of `void`.");
+
+            var firstExpression = TestHelpers.GetExpressions(method).FirstOrDefault();
+            Assert.True(firstExpression.ToString().Contains("Navigation.PushModalAsync"), "`NavigationPageView.xaml.cs`'s `PopModal` method does not contain a call to `Navigation.PushModalAsync` with an argument of a newly instantiated `PrivacyModal`.");
+            Assert.True(firstExpression.ToString().Contains("await"), "`NavigationPageView.xaml.cs`'s `PopModal` method's call to `Navigation.PushModalAsync` needs to be preceded with an `await`.");
+            Assert.True(firstExpression.ToString().Contains("PrivacyModal"), "`NavigationPageView.xaml.cs`'s `PopModal` method's call to `Navigation.PushModalAsync` should accept a newly instantiated `new PrivacyModal`.");
+
+            var constructor = TestHelpers.GetConstructors(ast).FirstOrDefault();
+            Assert.True(constructor.ToString().Contains("PopModal"), "`NavigationPageView.xaml.cs`'s constructor does not contain a call to `PopModal` after it's call to `InitializeComponent`.");
+        }
+    }
+}
--- XAMLInXamarinForms/XAMLInXamarinForms.Tests/NavigationPageViewTests.cs
+++ XAMLInXamarinForms/XAMLInXamarinForms.Tests/NavigationPageViewTests.cs
@@ -57,25 +57,6 @@ namespace XAMLInXamarinForms.Tests
             Assert.True(firstExpression.ToString().Contains("NutritionView"), "`NavigationPageView.xaml.cs`'s `NutritionButton_Click` method's call to `Navigation.PushAsync` should accept a newly instantiated `new NutritionView`.");
         }
 
-        [Fact(DisplayName = "Add Privacy Modal @add-privacy-modal")]
-        public void AddPrivacyModal()
-        {
-            var methods = TestHelpers.GetMethods(ast);
-            var method = methods?.FirstOrDefault(x => x.ToString().Contains("PopModal"));
-            Assert.True(method != null, "`NavigationPageView.xaml.cs` does not appear to contain a `private` `async` method `PopModal` with a `void` return type and has no parameters.");
-            Assert.True(method.Modifiers.Any(SyntaxKind.PrivateKeyword), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have the `private` modifier.");
-            Assert.True(method.Modifiers.Any(SyntaxKind.AsyncKeyword), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have the `async` modifier.");
-            Assert.True(method.ReturnType.ToString().Contains("void"), "`NavigationPageView.xaml.cs`'s `PopModal` method does not appear have a return type of `void`.");
-
-            var firstExpression = TestHelpers.GetExpressions(method).FirstOrDefault();
-            Assert.True(firstExpression.ToString().Contains("Navigation.PushModalAsync"), "`NavigationPageView.xaml.cs`'s `PopModal` method does not contain a call to `Navigation.PushModalAsync` with an argument of a newly instantiated `PrivacyModal`.");
-            Assert.True(firstExpression.ToString().Contains("await"), "`NavigationPageView.xaml.cs`'s `PopModal` method's call to `Navigation.PushModalAsync` needs to be preceded with an `await`.");
-            Assert.True(firstExpression.ToString().Contains("PrivacyModal"), "`NavigationPageView.xaml.cs`'s `PopModal` method's call to `Navigation.PushModalAsync` should accept a newly instantiated `new PrivacyModal`.");
-
-            var constructor = TestHelpers.GetConstructors(ast).FirstOrDefault();
-            Assert.True(constructor.ToString().Contains("PopModal"), "`NavigationPageView.xaml.cs`'s constructor does not contain a call to `PopModal` after it's call to `InitializeComponent`.");
-        }
-
         [Fact(DisplayName = "Add Click Events To View @add-click-events-to-view")]
         public void AddClickEventsToView()
         {
