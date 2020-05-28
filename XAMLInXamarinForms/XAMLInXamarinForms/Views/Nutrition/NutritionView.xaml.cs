4c67c97605affef6636cb2d15ab1d650e9b699c9
snychka <stefannychka@gmail.com>
2020-05-28 03:33:59 -0600

    finished mod2

---
+++ XAMLInXamarinForms/XAMLInXamarinForms/Views/Nutrition/NutritionView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XAMLInXamarinForms.Views.Nutrition
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NutritionView : TabbedPage
+    {
+        public NutritionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
